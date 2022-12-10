194c4a4cf29e2fb957b6b6e5c679bf2a410e1b9c
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2022-12-09 19:14:36 -0500

    Update portafolio.js

--- src/components/portafolio.js
+++ src/components/portafolio.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-undef */
 import React, { Component } from "react";
 import "./css/style.css";
 import Card from "react-bootstrap/Card";
@@ -33,10 +32,10 @@ export default class Portafolio extends Component {
               />
 
               <Card.Body>
-                <Card.Title className="text-center bold h3 bg-success">
+                <Card.Title className="text-center bold h3 bg-success" style={{ fontFamily: "Raleway,bold" }}>
                   La Plaza Campesina
                 </Card.Title>
-                <Card.Text>
+                <Card.Text style={{ fontFamily: "Raleway,bold" }}>
                   <p>Desarrollo de aplicación Web</p>
                   <p>Proyecto Ciclo 4 (front end y Back end)</p>
                   <p>Presentado a Misión Tic 2022 UTP</p>
@@ -46,6 +45,7 @@ export default class Portafolio extends Component {
                   target="_blank"
                   href="https://la-plaza-campesina-2022-qkbcxrtnl-paolapachecomoreno-gmailcom.vercel.app/"
                   variant="success"
+                  className="mx-3"
                 >
                   Ver Página
                 </Button>{" "}
@@ -60,6 +60,7 @@ export default class Portafolio extends Component {
                   target="_blank"
                   href="https://www.youtube.com/watch?v=NEpJmuFagwE"
                   variant="success"
+                  className="mx-3"
                 >
                   Ver Video
                 </Button>
@@ -89,10 +90,10 @@ export default class Portafolio extends Component {
                 className="img-thumbnail"
               />
               <Card.Body>
-                <Card.Title className="text-center bold h3 bg-success ">
+                <Card.Title className="text-center bold h3 bg-success " style={{ fontFamily: "Raleway,bold" }}>
                   CRUD de La Plaza Campesina
                 </Card.Title>
-                <Card.Text>
+                <Card.Text style={{ fontFamily: "Raleway,bold" }}>
                   <p>Desarrollo de Software (CRUD de productos Agricolas)</p>
                   <p>Proyecto Ciclo 3 (front end y Back end)</p>
                   <p>Presentado a Misión Tic 2022 UTP</p>
@@ -109,6 +110,7 @@ export default class Portafolio extends Component {
                   target="_blank"
                   href="https://youtu.be/prNclF8s5nU"
                   variant="success"
+                  className="mx-3"
                 >
                   Ver Video
                 </Button>
@@ -133,10 +135,10 @@ export default class Portafolio extends Component {
                 className="img-thumbnail"
               />
               <Card.Body>
-                <Card.Title className="text-center bold h3 bg-success">
+                <Card.Title className="text-center bold h3 bg-success" style={{ fontFamily: "Raleway,bold" }}>
                   Otros Proyectos
                 </Card.Title>
-                <Card.Text>
+                <Card.Text style={{ fontFamily: "Raleway,bold" }}>
                   Proyectos realizados con lenguajes tales como: Python, Js,
                   Java, React-Boostrap, Sql, Css, entre otros.
                 </Card.Text>
@@ -168,10 +170,10 @@ export default class Portafolio extends Component {
                 className="img-thumbnail"
               />
               <Card.Body>
-                <Card.Title className="text-center bold h3 bg-success">
+                <Card.Title className="text-center bold h3 bg-success" style={{ fontFamily: "Raleway,bold" }}>
                   Proyecto grupal(La plaza)
                 </Card.Title>
-                <Card.Text>
+                <Card.Text style={{ fontFamily: "Raleway,bold" }}>
                   Proyectos realizados con lenguajes tales como: Python, Js,
                   Java, React-Boostrap, Sql, Css, entre otros.
                 </Card.Text>
@@ -179,6 +181,7 @@ export default class Portafolio extends Component {
                   target="_blank"
                   href="https://github.com/La-plaza"
                   variant="warning"
+                  
                 >
                   Ver Repositorio Grupal
                 </Button>
