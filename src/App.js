dd3fba6a470b3fe339dd4dec4225bdb147e1dcb9
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2022-12-12 10:54:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ function App() {
   
   <footer className="footer"
      style={{
-      backgroundImage: `url("/footer.PNG")`,
+      backgroundImage: `url("/footer2.JPG")`,
       backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
       backgroundSize: "cover",
