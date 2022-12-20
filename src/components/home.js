2ba7bbca0d74985e2fd3d8d821f4df34f4ff6055
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2022-12-20 15:17:32 -0500

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -12,9 +12,9 @@ export default class Home extends Component {
           <Carousel.Item>
             <img
             alt="First slide"
-            src="https://scontent-bog1-1.xx.fbcdn.net/v/t31.18172-8/906008_1651281781754103_3090316986419273229_o.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeG1iuAYSJOmyNP2J16K_sUym_R9hRweR_ub9H2FHB5H--YV12SH3i1OSIPuNx6AvZbClqD5LPww5L6xa5whc_y-&_nc_ohc=GnDM8Jt5oFwAX_bkflK&tn=d1OULQvvQF9lcXK1&_nc_ht=scontent-bog1-1.xx&oh=00_AfCvayb3aj3E3cG_dmYG8r-hgZOkFa4MFi-2eWIyR0EpRw&oe=63B78C45" 
-             className=" img-thumbnail my-3 rounded-4 d-flex justify-content-center form-wrapper" 
-        style={{ height: '25rem', width: '40rem' }}
+            src="/piña.JPG"
+            className=" img-thumbnail my-3 rounded-4 d-flex justify-content-center form-wrapper" 
+            style={{ height: '25rem', width: '40rem' }}
               
             />
             <Carousel.Caption >
@@ -26,10 +26,10 @@ export default class Home extends Component {
           <Carousel.Item>
             <img
            alt="Second slide"
-           src="https://scontent-bog1-1.xx.fbcdn.net/v/t31.18172-8/13064707_1751577195057894_7737625205970002632_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEoWOnHXBzgoMGIK2Mu8GDTvkAUPpFvFPO-QBQ-kW8U8wRDG5IxErpkkeGE98770MbQ9uZjAD7_5AodvU5Za-bn&_nc_ohc=nPwt0fafkiYAX-_bvvq&_nc_ht=scontent-bog1-1.xx&oh=00_AfBtqM9N8fz5ptkSH6HijApb3eN6bl1Z45U6ffHtmfQZYQ&oe=63B78695" 
+           src="/caballoflores.JPG"
            className=" img-thumbnail my-3 rounded-4 d-flex justify-content-center form-wrapper"
               //className=" lg-10;"  
-        style={{ height: '25rem', width: '40rem' }}       
+           style={{ height: '25rem', width: '40rem' }}       
               
             />
           
@@ -38,9 +38,9 @@ export default class Home extends Component {
           <Carousel.Item>
             <img
            alt="Third slide"
-           src="https://scontent-bog1-1.xx.fbcdn.net/v/t31.18172-8/11792147_1659105587638389_4576592262907434646_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeEjBe4AqK_NIBN57bmTXo-8SIsO4vmc1Y9Iiw7i-ZzVjwQKqQap9oahJy7uDyTYvFh1cUp_5biV6aZ67Qu3PP8S&_nc_ohc=Nai0xmGLPmIAX9C0UWa&_nc_ht=scontent-bog1-1.xx&oh=00_AfAAyQIFAtU9dKT34FUcXKE-9-YRCXMEzCKIhMxxUWF1yg&oe=63B7842E"  
-            className=" img-thumbnail my-3 rounded-4 d-flex justify-content-center form-wrapper" 
-        style={{ height: '25rem', width: '40rem' }}
+           src="/casaflores.JPG"
+          className=" img-thumbnail my-3 rounded-4 d-flex justify-content-center form-wrapper" 
+          style={{ height: '25rem', width: '40rem' }}
               
             />
           </Carousel.Item>
