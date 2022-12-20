c0c0c7a5505016b628fb22b7e9e0bc96804692e0
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2022-12-20 14:35:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" Style={"color: #eed757;"} className="bg-dark"/>
         <Navbar.Collapse  id="responsive-navbar-nav"  >
           <Nav  id="opciones" Style={"color: #eed757;"}>
-            <Nav.Link Style={"color: rgba(14, 10, 0, 0.87); styles.subrayado"}className="bx- bx-user fw-bold" href="/perfil">Mi perfil profesional</Nav.Link>
+            <Nav.Link Style={"color: rgba(14, 10, 0, 0.87);"}className="bx- bx-user fw-bold" href="/perfil"><u>Mi perfil profesional</u></Nav.Link>
             <Nav.Link Style={"color: rgba(14, 10, 0, 0.87);"} className="bx bx-file-blank fw-bold  " href="/education">Educación</Nav.Link>
             <Nav.Link Style={"color: rgba(14, 10, 0, 0.87);"} className="bx bx-book-content fw-bold " href="/portafolio">Portafolio</Nav.Link>
             <Nav.Link Style={"color: rgba(14, 10, 0, 0.87);"} className="bx bx-server fw-bold  "  href="/habilidades">Habilidades</Nav.Link>
