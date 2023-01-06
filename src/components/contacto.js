071ed4222607387468ac6bf07afec862bcb8074c
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2023-01-05 23:58:39 -0500

    Update contacto.js

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -4,15 +4,15 @@ import "./css/style.css";
 export default class Contacto extends Component {
   render() {
     return (
-      <div class="container" style={{ fontFamily: "Raleway,bold" }}>
+      <div class="container" Style={{ fontFamily: "Raleway,bold" }}>
         <div class="section-title  text-center mt-5  ">
-          <h2 style={"color: rgb(197, 184, 70);"}}> CONTACTO</h2>
-          <p style={" color:     #e79c2b"} >
+          <h2 Style={"color:  #4e3003"}> CONTACTO</h2>
+          <p Style={" color:     #e79c2b"} >
             Si deseas contactarme, lo puedes realizar mediante Email o mediante
             teléfono.
           </p>
 
-          <p style={"color: rgb(197, 184, 70);"}>GRACIAS POR VISITAR MI PÁGINA .</p>
+          <p Style={"color: #7a4b05"}>GRACIAS POR VISITAR MI PÁGINA .</p>
         </div>
 
         <div class="row" data-aos="fade-in">
@@ -20,20 +20,20 @@ export default class Contacto extends Component {
             <div class="info">
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
-                <h4 style={"color: rgb(197, 184, 70);"}>Ubicación</h4>
-                <p style={" color:   #f7ac0b"}>Cundinamarca, Bogotá,</p>
+                <h4 Style={"color:  #7a4b05"}>Ubicación</h4>
+                <p Style={" color:   #f7ac0b"}>Cundinamarca, Bogotá,</p>
               </div>
 
               <div class="email">
                 <i class="bi bi-envelope"></i>
-                <h4 style={"color: rgb(197, 184, 70);"}>Email:</h4>
-                <p style={" color:  #e79c2b"}>paolapacheco.moreno@gmail.com</p>
+                <h4 Style={"color: #7a4b05"}>Email:</h4>
+                <p Style={" color:  #e79c2b"}>paolapacheco.moreno@gmail.com</p>
               </div>
 
               <div class="phone">
                 <i class="bi bi-phone"></i>
-                <h4 style={"color: rgb(197, 184, 70);"}>Teléfono:</h4>
-                <p style={" color:   #e79c2b"}>+57 3209887626</p>
+                <h4 Style={"color: #7a4b05"}>Teléfono:</h4>
+                <p Style={" color:   #e79c2b"}>+57 3209887626</p>
               </div>
             </div>
           </div>
@@ -46,7 +46,7 @@ export default class Contacto extends Component {
             >
               <div class="row">
                 <div class="form-group col-md-6">
-                  <label style={" color:  #e79c2b"} for="name">
+                  <label Style={" color:  #e79c2b"} for="name">
                     Nombre
                   </label>
                   <input
@@ -59,7 +59,7 @@ export default class Contacto extends Component {
                   />
                 </div>
                 <div class="form-group col-md-6">
-                  <label style={" color:  #e79c2b"} for="name">
+                  <label Style={" color:  #e79c2b"} for="name">
                     Email
                   </label>
                   <input
@@ -73,7 +73,7 @@ export default class Contacto extends Component {
                 </div>
               </div>
               <div class="form-group">  
-                <label style={" color:  #e79c2b"} for="name">
+                <label Style={" color:  #e79c2b"} for="name">
                   Asunto
                 </label>
                 <input
@@ -86,7 +86,7 @@ export default class Contacto extends Component {
                 />
               </div>
               <div class="form-group">
-                <label style={" color:  #e79c2b"} for="name">
+                <label Style={" color:  #e79c2b"} for="name">
                   Mensaje
                 </label>
                 <textarea
