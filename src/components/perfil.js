c4b1894d7e3f11dde1a1843ca0c829e61e3fd61b
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2025-06-19 16:07:15 -0500

    Update perfil.js

--- src/components/perfil.js
+++ src/components/perfil.js
@@ -54,7 +54,7 @@ export default class Perfil extends Component {
             className="img-thumbnail rounded-4 text-justify mx-2 mt-2 my-3 fw-bold"
           >
             <p>Soy Licenciada en Matemáticas y Estadística y Tecnóloga en Análisis y Desarrollo de Software, con formación en Desarrollo de Software,
-              Análisis y Visualización de Datos a través del programa Misión TIC 2022 (MinTIC) </p>
+              Análisis y Visualización de Datos a través del programa Misión TIC 2022 (MinTIC). </p>
                 
 
                <p> Cuento con más de 3 años de experiencia como analista de datos, con manejo de herramientas como Excel, Power BI, SQL, R, Python, MongoDB,
