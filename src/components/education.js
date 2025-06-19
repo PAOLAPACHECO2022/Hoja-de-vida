5cd1cf4128b13c85e17ba6fb2640e0a5bdae865f
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2025-06-19 01:07:35 -0500

    Update education.js

--- src/components/education.js
+++ src/components/education.js
@@ -1472,7 +1472,7 @@ export default function Education() {
                 striped
                 bordered
                 hover
-                className="img-thumbnail w-100 my-2 mt-2 d-flex justify-content-center fform-wrapper"                
+                className="img-thumbnail mt-2 justify-content-center form-wrapper"                    
               >
                 <thead class="bg-light form-wrapper rounded-4 text-center">
                   <tr class="form-wrapper">
