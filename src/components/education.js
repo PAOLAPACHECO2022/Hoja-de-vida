7a84a89d4be698ab35e7dfef92514944dac34dd2
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2025-06-18 21:45:48 -0500

    Update education.js

--- src/components/education.js
+++ src/components/education.js
@@ -682,7 +682,7 @@ export default function Education() {
           <Accordion.Item
             eventKey="1"
             style={{
-              backgroundImage: `url("./alas.PNG")`,
+              backgroundImage: `url("./estrellas.jpg")`,
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
               backgroundSize: "cover",
