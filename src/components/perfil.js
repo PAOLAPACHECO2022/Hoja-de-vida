d01a3017982f49a885d7ec4db3b0e713333d4c3f
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2025-06-19 20:54:49 -0500

    Update perfil.js

--- src/components/perfil.js
+++ src/components/perfil.js
@@ -6,7 +6,7 @@ export default class Perfil extends Component {
     return (
       <div
         style={{
-          backgroundImage: `url("./nueva2.PNG")`,
+          backgroundImage: `url("./nueva3.PNG")`,
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
