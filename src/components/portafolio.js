6db33c72e597d20fcacce8b4625525a6477edfe6
PAOLAPACHECO2022 <105870132+PAOLAPACHECO2022@users.noreply.github.com>
2025-06-21 11:41:11 -0500

    Update portafolio.js

--- src/components/portafolio.js
+++ src/components/portafolio.js
@@ -9,7 +9,7 @@ export default class Portafolio extends Component {
   render() {
     return (
       <Row xs={1} md={2} className="g-4  mt-5">
-      {Array.from({ length: 1 }).map((_, idx) => (
+        {Array.from({ length: 1 }).map((_, idx) => (
           <Col key={idx}>
             <Card
               className="img-thumbnail rounded-1"
@@ -21,71 +21,74 @@ export default class Portafolio extends Component {
                 border: "2px solid #ffffff",
                 padding: 0,
                 margin: 0
-                
               }}
             >
-              
-               <Card.Img
+              <Card.Img
                 variant="top"
                 src="./colegio.PNG.jpg"
                 className="img-thumbnail"
               />
-                <Card.Img
+              <Card.Img
                 variant="top"
                 src="./colegio1.jpg"
                 className="img-thumbnail"
               />
               <Card.Body>
-                <Card.Title className="text-center bold h2 bg-success img-thumbnail rounded-3" style={{ fontFamily: "Raleway,bold" }}>
-                 Software MERN "School.Info."
+                <Card.Title
+                  className="text-center bold h2 bg-success img-thumbnail rounded-3"
+                  style={{ fontFamily: "Raleway,bold" }}
+                >
+                  Software MERN "School.Info."
                 </Card.Title>
                 <Card.Text style={{ fontFamily: "Raleway,bold" }}>
-                   <p>Desarrollo de aplicación Web</p>
-                   <p>Diciembre 2024</p>
-                   <p className="text-justify" >"El software School.info busca mejorar la eficiencia operativa, académica y 
-                       comunicativa de cualquier institución y al mismo tiempo promover un 
-                       entorno de aprendizaje colaborativo y accesible. 
-                       Este sistema facilita los procesos y/o tareas administrativas tales como, 
-                       inscripción de estudiantes y docentes, control de asistencia, generación de 
-                       reportes académico y convivenciales. Permite la creación, seguimiento y 
-                       evaluación de planes de estudio y programas académico, así mismo permite 
-                       establecer canales de comunicación entre docentes, estudiantes y personal 
-                       administrativo, facilita la interacción entre la institución educativa y padres de familia acerca del 
-                       desempeño académico y comportamiento de los estudiantes. "</p>
-                   
+                  <p>Desarrollo de aplicación Web</p>
+                  <p>Diciembre 2024</p>
+                  <p className="text-justify">
+                    "El software School.info busca mejorar la eficiencia
+                    operativa, académica y comunicativa de cualquier institución
+                    y al mismo tiempo promover un entorno de aprendizaje
+                    colaborativo y accesible. Este sistema facilita los procesos
+                    y/o tareas administrativas tales como, inscripción de
+                    estudiantes y docentes, control de asistencia, generación de
+                    reportes académico y convivenciales. Permite la creación,
+                    seguimiento y evaluación de planes de estudio y programas
+                    académico, así mismo permite establecer canales de
+                    comunicación entre docentes, estudiantes y personal
+                    administrativo, facilita la interacción entre la institución
+                    educativa y padres de familia acerca del desempeño académico
+                    y comportamiento de los estudiantes. "
+                  </p>
                 </Card.Text>
-               <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-2 mt-3">
-  <Button
-    target="_blank"
-    href="https://drive.google.com/file/d/13d1w1MDf2Nz07XhImvtH4Di3BiqndltC/view?usp=sharing"
-    variant="warning"
-  >
-    Manual de Usuario
-  </Button>
+                <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-2 mt-3">
+                  <Button
+                    target="_blank"
+                    href="https://drive.google.com/file/d/13d1w1MDf2Nz07XhImvtH4Di3BiqndltC/view?usp=sharing"
+                    variant="warning"
+                  >
+                    Manual de Usuario
+                  </Button>
 
-  <Button
-    target="_blank"
-    href="https://github.com/PAOLAPACHECO2022/schoolinfoserver.git"
-    variant="warning"
-  >
-    Repositorio
-  </Button>
+                  <Button
+                    target="_blank"
+                    href="https://github.com/PAOLAPACHECO2022/schoolinfoserver.git"
+                    variant="warning"
+                  >
+                    Repositorio
+                  </Button>
 
-  <Button
-    target="_blank"
-    href="https://schoolinfocliente.onrender.com/"
-    variant="warning"
-  >
-    Ver Aplicación
-  </Button>
-</div>
-
-       
+                  <Button
+                    target="_blank"
+                    href="https://schoolinfocliente.onrender.com/"
+                    variant="warning"
+                  >
+                    Ver Aplicación
+                  </Button>
+                </div>
               </Card.Body>
             </Card>
           </Col>
         ))}
-   {Array.from({ length: 1 }).map((_, idx) => (
+        {Array.from({ length: 1 }).map((_, idx) => (
           <Col key={idx}>
             <Card
               className="img-thumbnail rounded-1"
@@ -97,21 +100,19 @@ export default class Portafolio extends Component {
                 border: "2px solid #ffffff",
                 padding: 0,
                 margin: 0
-                
               }}
             >
-              
-               <Card.Img
+              <Card.Img
                 variant="top"
                 src="./t2.JPG"
                 className="img-thumbnail"
               />
-                <Card.Img
+              <Card.Img
                 variant="top"
                 src="./t1.JPG"
                 className="img-thumbnail"
               />
-                <Card.Img
+              <Card.Img
                 variant="top"
                 src="./ResultadoML.jpg"
                 className="img-thumbnail"
@@ -119,91 +120,94 @@ export default class Portafolio extends Component {
               <Card.Body>
                 <Card.Title
                   className="text-center bg-success img-thumbnail rounded-3"
-                   style={{
+                  style={{
                     fontFamily: "Raleway",
-                    fontSize: "2rem", // 
-                    textAlign: "justify", // 
-                    fontWeight: "bold",   // 
-                     }}
-                      >
-                     “Factores demográficos, socioeconómicos y académicos que influyen sobre los resultados del componente genérico de la prueba Saber Pro.
-                     Caso: Administración de Empresas Agropecuarias de la Universidad Pedagógica y Tecnológica de Colombia, seccional Duitama”.
-                     </Card.Title>
+                    fontSize: "1.25rem", //
+                    textAlign: "justify", //
+                    fontWeight: "bold" //
+                  }}
+                >
+                  “Factores demográficos, socioeconómicos y académicos que
+                  influyen sobre los resultados del componente genérico de la
+                  prueba Saber Pro. Caso: Administración de Empresas
+                  Agropecuarias de la Universidad Pedagógica y Tecnológica de
+                  Colombia, seccional Duitama”.
+                </Card.Title>
 
                 <Card.Text style={{ fontFamily: "Raleway,bold" }}>
-                  Proyecto de investigación (Modelo de regresión logística para la predicción de los puntajes de la prueba saber Pro)
+                  Proyecto de investigación (Modelo de regresión logística para
+                  la predicción de los puntajes de la prueba saber Pro)
                 </Card.Text>
                 <Button
                   target="_blank"
                   href="https://drive.google.com/file/d/1Kla1LvU8mzuNdSFvrIUOMkaDy0OCWKb4/view?usp=sharing"
                   variant="warning"
-                  
                 >
-                  Ver Proyecto de investigación 
+                  Ver Proyecto de investigación
                 </Button>
               </Card.Body>
             </Card>
           </Col>
         ))}
         {Array.from({ length: 1 }).map((_, idx) => (
-  <Col key={idx}>
-    <Card
-      className=" img-thumbnail rounded-1"
-      style={{
-        backgroundImage: `url("./alas.PNG")`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        border: "2px solid #ffffff",
-        padding: 0,
-        margin: 0
-      }}
-    >
-      <Card.Img
-        variant="top"
-        src="./laplaza.PNG"
-        className="img-thumbnail rounded-0"
-        style={{ margin: 0, padding: 0 }}
-      />
-      <Card.Img
-        variant="top"
-        src="./laplaza2.PNG"
-        className="rounded-0 img-thumbnail"
-        style={{ margin: 0, padding: 0 }}
-      />
+          <Col key={idx}>
+            <Card
+              className=" img-thumbnail rounded-1"
+              style={{
+                backgroundImage: `url("./alas.PNG")`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                border: "2px solid #ffffff",
+                padding: 0,
+                margin: 0
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src="./laplaza.PNG"
+                className="img-thumbnail rounded-0"
+                style={{ margin: 0, padding: 0 }}
+              />
+              <Card.Img
+                variant="top"
+                src="./laplaza2.PNG"
+                className="rounded-0 img-thumbnail"
+                style={{ margin: 0, padding: 0 }}
+              />
 
-      <Card.Body style={{ padding: "1rem" }}>
-        <Card.Title
-          className="text-center h3 bg-success rounded-3"
-          style={{ fontFamily: "Raleway, bold" }}
-        >
-          La Plaza Campesina
-        </Card.Title>
-        <Card.Text style={{ fontFamily: "Raleway, bold" }}>
-          <p>Desarrollo de aplicación Web</p>
-          <p>Proyecto Ciclo 4 (front end y Back end)</p>
-          <p>Presentado a Misión Tic 2022 UTP</p>
-          <p>28 de Noviembre 2022</p>
-        </Card.Text>
-        <Button
-          target="_blank"
-          href="https://github.com/PAOLAPACHECO2022/LA--PLAZA--CAMPESINA--2022.git"
-          variant="warning"
-        >
-          Ver Repositorio
-        </Button>
-        <Button
-          target="_blank"
-          href="https://www.youtube.com/watch?v=NEpJmuFagwE"
-          variant="success"
-          className="mx-3"
-        >
-          Ver Video
-        </Button>
-      </Card.Body>
-    </Card>
-  </Col>
-))}
+              <Card.Body style={{ padding: "1rem" }}>
+                <Card.Title
+                  className="text-center h3 bg-success rounded-3"
+                  style={{ fontFamily: "Raleway, bold" }}
+                >
+                  La Plaza Campesina
+                </Card.Title>
+                <Card.Text style={{ fontFamily: "Raleway, bold" }}>
+                  <p>Desarrollo de aplicación Web</p>
+                  <p>Proyecto Ciclo 4 (front end y Back end)</p>
+                  <p>Presentado a Misión Tic 2022 UTP</p>
+                  <p>28 de Noviembre 2022</p>
+                </Card.Text>
+                <Button
+                  target="_blank"
+                  href="https://github.com/PAOLAPACHECO2022/LA--PLAZA--CAMPESINA--2022.git"
+                  variant="warning"
+                >
+                  Ver Repositorio
+                </Button>
+                <Button
+                  target="_blank"
+                  href="https://www.youtube.com/watch?v=NEpJmuFagwE"
+                  variant="success"
+                  className="mx-3"
+                >
+                  Ver Video
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
 
         {Array.from({ length: 1 }).map((_, idx) => (
           <Col key={idx}>
@@ -219,21 +223,21 @@ export default class Portafolio extends Component {
                 margin: 0
               }}
             >
-       
               <Card.Img
                 variant="top"
                 src="./lafinca.PNG"
                 className="img-thumbnail rounded-1"
-                
               />
               <Card.Img
                 variant="top"
                 src="./crearp.PNG"
                 className="img-thumbnail rounded-1"
-                
               />
               <Card.Body>
-                <Card.Title className="text-center bold h3 bg-success  img-thumbnail rounded-3" style={{ fontFamily: "Raleway,bold" }}>
+                <Card.Title
+                  className="text-center bold h3 bg-success  img-thumbnail rounded-3"
+                  style={{ fontFamily: "Raleway,bold" }}
+                >
                   CRUD de La Plaza Campesina
                 </Card.Title>
                 <Card.Text style={{ fontFamily: "Raleway,bold" }}>
@@ -264,7 +268,7 @@ export default class Portafolio extends Component {
         {Array.from({ length: 1 }).map((_, idx) => (
           <Col key={idx}>
             <Card
-               className="img-thumbnail rounded-1"
+              className="img-thumbnail rounded-1"
               style={{
                 backgroundImage: `url("./alas.PNG")`,
                 backgroundPosition: "center",
@@ -279,10 +283,12 @@ export default class Portafolio extends Component {
                 variant="top"
                 src="./lengua.PNG"
                 className="img-thumbnail rounded-1"
-               
               />
               <Card.Body>
-                <Card.Title className="text-center bold h3 bg-success img-thumbnail rounded-3" style={{ fontFamily: "Raleway,bold" }}>
+                <Card.Title
+                  className="text-center bold h3 bg-success img-thumbnail rounded-3"
+                  style={{ fontFamily: "Raleway,bold" }}
+                >
                   Otros Proyectos
                 </Card.Title>
                 <Card.Text style={{ fontFamily: "Raleway,bold" }}>
@@ -303,7 +309,7 @@ export default class Portafolio extends Component {
         {Array.from({ length: 1 }).map((_, idx) => (
           <Col key={idx}>
             <Card
-               className="img-thumbnail rounded-1"
+              className="img-thumbnail rounded-1"
               style={{
                 backgroundImage: `url("./alas.PNG")`,
                 backgroundPosition: "center",
@@ -320,7 +326,10 @@ export default class Portafolio extends Component {
                 className="img-thumbnail rounded-1"
               />
               <Card.Body>
-                <Card.Title className="text-center bold h3 bg-success  img-thumbnail rounded-3" style={{ fontFamily: "Raleway,bold" }}>
+                <Card.Title
+                  className="text-center bold h3 bg-success  img-thumbnail rounded-3"
+                  style={{ fontFamily: "Raleway,bold" }}
+                >
                   Proyecto grupal(La plaza)
                 </Card.Title>
                 <Card.Text style={{ fontFamily: "Raleway,bold" }}>
@@ -331,7 +340,6 @@ export default class Portafolio extends Component {
                   target="_blank"
                   href="https://github.com/La-plaza"
                   variant="warning"
-                  
                 >
                   Ver Repositorio Grupal
                 </Button>
@@ -339,8 +347,8 @@ export default class Portafolio extends Component {
             </Card>
           </Col>
         ))}
-        
-       </Row>
+      </Row>
     );
   }
 }
+
